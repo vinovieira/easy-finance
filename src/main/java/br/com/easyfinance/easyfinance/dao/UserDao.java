213ad1aed454a75d33b21c8c4d504d756f9b6e8e
vinovieira <127680187+vinovieira@users.noreply.github.com>
2024-12-12 19:48:34 -0400

    fix: login and others and feat: user save in session

--- src/main/java/br/com/easyfinance/easyfinance/dao/UserDao.java
+++ src/main/java/br/com/easyfinance/easyfinance/dao/UserDao.java
@@ -3,5 +3,5 @@ package br.com.easyfinance.easyfinance.dao;
 import br.com.easyfinance.easyfinance.model.User;
 
 public interface UserDao {
-    boolean validateUser(User user);
+    User validateUser(User user);
     }
